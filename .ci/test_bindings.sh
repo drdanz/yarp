dc9a1ec961caa5242eed8237a0c91a5d269f898a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-04-03 20:24:26 +0200

    move to script

---
+++ .ci/test_bindings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+    echo "Usage: $0 [language]"
+fi
+
+_lang=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+export YARP_BINDINGS_CMAKE_OPTIONS="-DYARP_COMPILE_BINDINGS=ON -DCREATE_${_lang}=ON"
+echo "Building ${_lang} bindings"
+
+echo "Nested build"
+(cd build; cmake . ${YARP_BINDINGS_CMAKE_OPTIONS})
+(cd build; cmake --build . --config ${YARP_CMAKE_BUILD_TYPE})
+
+echo "Out of source build"
+mkdir -p bindings/build
+(cd bindings/build; cmake -G"${YARP_CMAKE_GENERATOR}" -DCMAKE_BUILD_TYPE=${YARP_CMAKE_BUILD_TYPE} -DYARP_DIR=${PWD}/../../build ${YARP_BINDINGS_CMAKE_OPTIONS})
+(cd bindings/build; cmake --build . --config ${YARP_CMAKE_BUILD_TYPE})
