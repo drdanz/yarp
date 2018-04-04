81135ad2b65bb10ed5e7f191d016d736ff570843
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-04-04 17:21:14 +0200

    ...

--- .ci/configure.sh
+++ .ci/configure.sh
@@ -5,6 +5,9 @@
 export YARP_CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=${YARP_INSTALL_PREFIX}"
 export YARP_CMAKE_OPTIONS="${YARP_CMAKE_OPTIONS} -DENABLE_yarpcar_h264:BOOL=ON"
 
+# FIXME: Use CMAKE_C_COMPILER_LAUNCHER and CMAKE_CXX_COMPILER_LAUNCHER instead
+#        of changing the path as soon as CMake > 3.4 is required.
+
 # Print cmake build flags
 echo "--------------------     BEGIN CMAKE FLAGS    --------------------"
 echo " \
@@ -24,3 +27,8 @@ cmake -H"${YARP_SOURCE_DIR}" \
       -DCMAKE_BUILD_TYPE=${YARP_CMAKE_BUILD_TYPE} \
       ${YARP_CMAKE_OPTIONS} \
       ${YARP_EXTRA_CMAKE_OPTIONS}
+
+# Print cmake cache
+echo "--------------------     BEGIN CMAKE CACHE    --------------------"
+cat ${YARP_BINARY_DIR}/CMakeCache.txt
+echo "--------------------      END CMAKE CACHE     --------------------"
