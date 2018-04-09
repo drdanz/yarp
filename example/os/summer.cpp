515fda2e55ad81dde6ecd75de577ffb76a812705
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-04-09 15:32:48 +0200

    Cleanup

--- example/os/summer.cpp
+++ example/os/summer.cpp
@@ -4,33 +4,36 @@
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
  */
 
-#include <yarp/os/all.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Bottle.h>
 #include <iostream>
 
-using namespace std;
-using namespace yarp::os;
+using yarp::os::Network;
+using yarp::os::BufferedPort;
+using yarp::os::Bottle;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     Network yarp;
     BufferedPort<Bottle> port;
     port.open("/summer");
     while (true) {
-        cout << "waiting for input" << endl;
-        Bottle *input = port.read();
-        if (input!=NULL) {
-            cout << "got " << input->toString().c_str() << endl;
+        std::cout << "waiting for input" << std::endl;
+        Bottle* input = port.read();
+        if (input != NULL) {
+            std::cout << "got " << input->toString().c_str() << std::endl;
             double total = 0;
-            for (int i=0; i<input->size(); i++) {
+            for (int i = 0; i < input->size(); i++) {
                 total += input->get(i).asDouble();
             }
             Bottle& output = port.prepare();
             output.clear();
             output.addString("total");
             output.addDouble(total);
-            cout << "writing " << output.toString().c_str() << endl;
+            std::cout << "writing " << output.toString().c_str() << std::endl;
             port.write();
         }
     }
     return 0;
 }
-
